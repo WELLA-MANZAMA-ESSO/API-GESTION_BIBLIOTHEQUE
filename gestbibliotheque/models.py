909e7368072b5ea0467b9bff9c0236ec7ee1dc5e
Mazar-christian <christianmazar745@gmail.com>
2026-07-26 21:06:57 +0000

    ajout des permissions dans l'app gestbibliotheque et insertions des données dans la base de données

--- gestbibliotheque/models.py
+++ gestbibliotheque/models.py
@@ -30,12 +30,12 @@ class Categorie(models.Model):
 class Livre(models.Model):
     titre=models.CharField(max_length=50)
     isbn=models.CharField(max_length=10,unique=True, null=True)
-    date_ajout=models.DateTimeField(auto_created=True)
+    date_ajout=models.DateTimeField(auto_now_add=True)
     date_termine=models.DateTimeField(blank=True, null=True)
     emprunt=models.BooleanField(default=False)
     date_emprunt=models.DateTimeField(blank=True,null=True)
     date_retour=models.DateTimeField(blank=True,null=True)
 
     auteur=models.ForeignKey(Auteur, on_delete=models.CASCADE, related_name='auteur')
-    livre=models.ForeignKey(Categorie,on_delete=models.CASCADE,related_name='categorie')
+    categorie=models.ForeignKey(Categorie,on_delete=models.CASCADE,related_name='categorie')
     utilisateur=models.ForeignKey(Utilisateur,on_delete=models.CASCADE,related_name='utilisateur')
