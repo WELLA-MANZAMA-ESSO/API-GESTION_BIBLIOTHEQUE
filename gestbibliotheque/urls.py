64e2d24b4c420f59f0eb99febca8c8539f4a0835
Mazar-christian <christianmazar745@gmail.com>
2026-07-26 19:50:11 +0000

    Ajout des serializers categorie,livre et utilisateur, mise en place des permissions personnalisées et  des serilaizers imbriqués sur le livreserializer

--- gestbibliotheque/urls.py
+++ gestbibliotheque/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path,include
 from rest_framework import routers
 
-from gestbibliotheque.views import AuteurView
+from gestbibliotheque.views import AuteurView,LivreView,CategorieView
 
 router=routers.SimpleRouter()
 router.register('auteur',AuteurView,basename='auteur')
+router.register('livre',LivreView,basename='livre')
+router.register('categorie',CategorieView,basename='categorie')
 
 urlpatterns = [
     path('',include(router.urls)),
--- utilisateurs/urls.py
+++ utilisateurs/urls.py
@@ -1,5 +1,12 @@
-from rest_framework.urls import path
+from django.urls import path,include
+from rest_framework import routers
+from utilisateurs.views import UtilisateurView
+
+router=routers.SimpleRouter()
+router.register('utilisateur',UtilisateurView,basename='utilisateur')
+
 
 urlpatterns = [
-    
+    path('',include(router.urls)),
+
 ]
